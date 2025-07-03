eb10e22ac7b853024fed8b9406fa4e48849467c5
Aryan-irl <aryanrajput1203@gmail.com>
2025-07-03 18:47:03 +0000

    datatpes

---
+++ 2datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all Js code as newer version
+
+// alert(3+3); we r using node js not browser
+
+console.log(3+3);
+
+
+console.log("aryan");
+
+let name="aryan"; // string
+let age=20;// number
+let isLoggedIn=true; //boolean
+
+//bigint 
+//STRING=>" "
+//Boolean=>true/false
+//number=>1,2,3,4,5,6,7,8,
+//null=>standalone value
+//undefined=>not defined yet
+
+console.log(typeof "aryan"); //string
+console.log(typeof null); //number
